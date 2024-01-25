01e18f5d82570ec733c7d73910d6eaac43372180
Gennadiy Samoylov <65565925+CantLover@users.noreply.github.com>
2024-01-26 00:57:24 +0300

    demo version

---
+++ cmd/pet-http-server/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "gennadiy"
+	password = "123"
+	dbname   = "appeals"
+)
+
+type Appeal struct {
+	authorName, authorLocation, authorMail, appealDate, appealText string
+}
+
+var db *sql.DB
+var connStr string = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+
+func createListTable(db *sql.DB) {
+	dbConErr := db.Ping()
+	if dbConErr != nil {
+		fmt.Println("smth wrong with ping in CREATE TABLE STEP")
+		log.Fatal(dbConErr)
+	}
+	_, createTableErr := db.Exec(`
+		CREATE TABLE IF NOT EXISTS appeals_list (
+			id SERIAL PRIMARY KEY,
+			authorName VARCHAR(255),
+			authorLocation VARCHAR(255),
+			authorMail VARCHAR(255),
+			appealDate VARCHAR(255),
+			appealText VARCHAR(255)
+		)
+	`)
+	if createTableErr != nil {
+		log.Fatal(createTableErr)
+
+	} else {
+		fmt.Println("Table created successfully")
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../../templates/home.html")
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "../../templates/form.html")
+
+	} else if r.Method == http.MethodPost {
+
+		if pingErr := db.Ping(); pingErr != nil {
+			fmt.Println("Error at writing to db")
+			log.Fatal(pingErr)
+		} else {
+			responseData := Appeal{
+				authorName:     r.FormValue("authorName"),
+				authorLocation: r.FormValue("authorLocation"),
+				authorMail:     r.FormValue("authorMail"),
+				appealDate:     time.Now().Format("2002-01-02 15:23"),
+				appealText:     r.FormValue("appealText"),
+			}
+
+			_, insertErr := db.Exec(`
+				INSERT INTO appeals_list (authorName, authorLocation, authorMail, appealDate, appealText)
+				VALUES ($1, $2, $3, $4, $5)
+			`, responseData.authorName,
+				responseData.authorLocation,
+				responseData.authorMail,
+				responseData.appealDate,
+				responseData.appealText,
+			)
+
+			if insertErr != nil {
+				fmt.Println("Error at insert")
+				http.ServeFile(w, r, "../../templates/form.html")
+				log.Fatal(insertErr)
+
+			} else {
+				http.ServeFile(w, r, "../../templates/list.html")
+			}
+
+		}
+
+	}
+
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../../templates/list.html")
+}
+
+func main() {
+	//connectToDB()
+	var dbConErr error
+	db, dbConErr = sql.Open("postgres", connStr)
+
+	if dbConErr != nil {
+		log.Println("Error at creating db")
+		log.Fatal(dbConErr)
+	} else {
+		fmt.Println("Successfully connected to the database")
+	}
+
+	dbConErr = db.Ping()
+	if dbConErr != nil {
+		fmt.Println("smth wrong with ping in MAIN")
+		log.Fatal(dbConErr)
+	}
+	defer db.Close()
+
+	createListTable(db)
+
+	//fmt.Println("hello")
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/list", listHandler)
+
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Starting server at port 8082")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module pet-http-server
+
+go 1.21.2
+
+require github.com/lib/pq v1.10.9
